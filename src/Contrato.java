fd8c906ec2e5aad10ca5444d720429c69c19923a
devluangabriel <zuemeromualdo44@gmail.com>
2025-05-02 13:36:19 -0300

    Poo Polimorfismo Java

---
+++ src/Contrato.java
@@ -0,0 +1,7 @@
+public class Contrato {
+
+    public void Polimorfismo(){
+        System.out.println("Pólimorfismo");
+    }
+
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,15 @@
+public class Pessoa extends Contrato {
+
+
+    @Override
+    public void Polimorfismo() {
+        System.out.println("Polimorfismo Sobre escrita.");
+    }
+
+
+    public void Polimorfismo(String one) {
+        System.out.println("Polimorfismo Sobre carga.");
+    }
+
+
+}
